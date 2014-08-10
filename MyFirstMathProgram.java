3ed4acf46b27d3d89ac18c4ea08d98607f9d556f
Md Alam <mdalam1218@gmail.com>
2014-08-09 23:58:35 -0400

    My first adding  project  in java

---
+++ MyFirstMathProgram.java
@@ -0,0 +1,15 @@
+package com.google.qa;
+
+public class MyFirstMathProgram {
+	public static void main(String[]args){
+		int firstNumber=5;
+		int secandNumber=10;
+		int total;
+		total=firstNumber+secandNumber;
+		
+		System.out.print("my total is:---");
+		System.out.println(total);
+		
+	}
+
+}
